e8a9afab0f79bcd296ef7bc4a89f580fb96a8383
Renze <merenze@gmail.com>
2017-08-21 10:46:40 -0500

    Fixed CraftBukkit dependency; ChatSerializer appears to import normally

--- src/main/java/io/github/merenze/reporttps/ReportTps.java
+++ src/main/java/io/github/merenze/reporttps/ReportTps.java
@@ -4,10 +4,14 @@ import java.util.Timer;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import net.minecraft.server.v1_12_R1.IChatBaseComponent;
+import net.minecraft.server.v1_12_R1.IChatBaseComponent.ChatSerializer;
+
 public class ReportTps extends JavaPlugin {
 	@Override
 	public void onEnable() {
-		Timer timer = new Timer();
-		timer.schedule(new TimerTitle(), 0, 30000);
+		String text = "Foo";
+		IChatBaseComponent chatTitle = ChatSerializer.a("{\"text\": \"" + text + "}");
+
 	}
 }
